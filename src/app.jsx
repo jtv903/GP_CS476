5292f3177398e7b0f9999e2bfab543018931d141
Jeet Patel <jeetgadhia5@gmail.com>
2024-07-09 19:57:42 -0600

    .

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './app.css';
-import Header from './components/Header';
-import ItemList from './components/Itemlist';
+import Header from './Header';
+import ItemList from './Itemlist';
 
 
 function App() {
