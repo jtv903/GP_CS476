8ccfead14e60ce5f4269f74a3ac82c2597e4180d
Jeet Patel <jeetgadhia5@gmail.com>
2024-07-09 19:55:15 -0600

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,41 +1,26 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import './main.css';
-import Header from './Header';
-import ItemList from './Itemlist';
-import Checkout from './Checkout';
+import './App.css';
+import Header from './components/Header';
+import ItemList from './components/Itemlist';
 
-function Home() {
-  const navigate = useNavigate();
-  const goToCheckout = () => {
-    navigate('/checkout');
-  };
-
-  return (
-    <div className="App">
-      <Header />
-      <div className="categories">
-        <button>Produce</button>
-        <button>Baby</button>
-        <button>Home Essentials</button>
-        <button>Meat</button>
-        <button>Clothing</button>
-        <button>Health</button>
-        <button>Beauty</button>
-        <button>Category</button>
-        <button onClick={goToCheckout}>Checkout</button>
-      </div>
-      <ItemList />
-    </div>
-  );
-}
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/checkout" element={<Checkout />} />
-    </Routes>
+    <div className="App">
+      <Header />
+          <div className="categories">
+            <button>Produce</button>
+            <button>Baby</button>
+            <button>Home Esentials</button>
+            <button>Meat</button>
+            <button>Clothing</button>
+            <button>Health</button>
+            <button>Beauty</button>
+            <button>Category#</button>
+          </div>
+  
+      <ItemList />
+    </div>
   );
 }
 
